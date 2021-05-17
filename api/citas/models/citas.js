0839c94c7f9a7c2e7e3f9413b8bd6fdbc0e30ae5
YPZ-24 <aylinypz@gmail.com>
2021-05-17 18:02:36 -0500

    citasMuttation

--- api/citas/models/citas.js
+++ api/citas/models/citas.js
@@ -1,11 +1,12 @@
 module.exports = {
     lifecycles: {
         async beforeUpdate(params, data) {
-            //data.email = 'Some fixed name';
-            const {enlace: oldLink} = await strapi.services.citas.findOne({id: data._id}, )
-            const {enlace: newLink} = data
-            if(oldLink!==newLink){
-                console.log("ENVIAR MENSAJE CON LINK DE LA ASESORIA")
+            if(data._id){
+                const {enlace: oldLink} = await strapi.services.citas.findOne({id: data._id}, )
+                const {enlace: newLink} = data
+                if(oldLink!==newLink){
+                    console.log("ENVIAR MENSAJE CON LINK DE LA ASESORIA")
+                }
             }
         },
     },
