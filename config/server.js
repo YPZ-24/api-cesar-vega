82355f5e5b7e44ad1b2b2581c7c8118ea64a9700
YPZ-24 <aylinypz@gmail.com>
2021-06-14 22:46:07 -0500

    urls server.js

--- config/server.js
+++ config/server.js
@@ -1,9 +1,16 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  admin: {
+  /*admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET'),
     },
-  },
+  },*/
+  url: 'http://cesarvega.com.mx/api',
+  admin: {
+    url: 'http://cesarvega.com.mx/dashboard',
+    auth: {
+      secret: env('ADMIN_JWT_SECRET'),
+    },
+  }
 });
