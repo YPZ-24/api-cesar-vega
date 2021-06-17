f03b2ae93f915b653aa66f7a28c4ca79e202bcca
YPZ-24 <aylinypz@gmail.com>
2021-06-17 16:23:48 -0500

    without video uploaded

--- api/citas/models/citas.js
+++ api/citas/models/citas.js
@@ -10,16 +10,20 @@ module.exports = {
                     const {email} = await strapi.query('user', 'users-permissions').findOne({id: idUser})
                     try{
                         const fechaFormatted = fecha.toLocaleString('es-MX')
+                        console.log(fecha)
+                        console.log(fechaFormatted)
                         const msj =     `<p>Gracias por agendar una cita, Actualización!</p>
                                         Asunto: ${asunto}<br/>
                                         Fecha: ${fechaFormatted}<br/>
                                         Enlace Actualizado: ${newLink}<br/>
                                         `        
-                        await sendEmail( {
+
+                        console.log(msj)
+                        /*await sendEmail( {
                             message: msj, 
                             receiver: email, 
                             subject: "CESAR VEGA | ASESORIA" 
-                        })
+                        })*/
                     }catch(error){
                         throw new Error(error)
                     }
