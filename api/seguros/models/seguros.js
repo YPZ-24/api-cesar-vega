1d08e7c47698160bc7af3f48e345b06e60afd981
YPZ-24 <aylinypz@gmail.com>
2022-01-25 21:20:16 -0600

    comment notifications

--- api/seguros/models/seguros.js
+++ api/seguros/models/seguros.js
@@ -2,6 +2,7 @@ const {sendNotificationByTokens} = require('../../../util/firebase')
 
 module.exports = {
     lifecycles: {
+        /*
         async afterUpdate(data) {
             if(data.published_at){
                 const devices = await strapi.query('dispositivos').model.find({},{token: 1, _id: 0})
@@ -13,6 +14,6 @@ module.exports = {
                 await strapi.services.notificaciones.create({titulo:title, contenido:body, tipo, relacion})
                 await sendNotificationByTokens({title, body, tokens:tokens})
             }
-        },
+        },*/
     },
 };
